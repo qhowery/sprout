497427ab0189c7abbcaf51531e6113dc24275634
Quon Howery <qhowery@princeton.edu>
2024-09-07 19:35:58 -0600

    ready

--- src/Navbar/RightContent/AuthButtons.tsx
+++ src/Navbar/RightContent/AuthButtons.tsx
@@ -20,7 +20,7 @@ const AuthButtons: React.FC = () => {
       </Button>
       <Button
         variant="solid"
-        height="28px"
+        height="29px"
         display={{ base: "none", sm: "flex" }}
         width={{ base: "70px", md: "110px" }}
         mr={2}
