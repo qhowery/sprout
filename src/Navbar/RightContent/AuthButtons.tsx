40186801ef44a365367a4332e2d6cbb4fd06bfe7
Quon Howery <qhowery@princeton.edu>
2024-09-07 19:37:55 -0600

    update button size

--- src/Navbar/RightContent/AuthButtons.tsx
+++ src/Navbar/RightContent/AuthButtons.tsx
@@ -20,7 +20,7 @@ const AuthButtons: React.FC = () => {
       </Button>
       <Button
         variant="solid"
-        height="29px"
+        height="28px"
         display={{ base: "none", sm: "flex" }}
         width={{ base: "70px", md: "110px" }}
         mr={2}
